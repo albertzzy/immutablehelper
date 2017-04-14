e54742bf366c348286ad3e2ac71f284da29f6a50
albertzzy <zhaozi_ye_hi@126.com>
2017-04-14 15:55:43 +0800

    update a bit

--- immutable-helper.js
+++ immutable-helper.js
@@ -1,26 +1,60 @@
 // update a deep plain-obj simple helper
+/*
+	['a','b']
+	['a','b:1']
+
+	set - 单个值（plainobject key-val[merge],array val）
+	add - array push
+	delet - array key delete,plainobject key delete
+*/
+
+function isObject(obj){
+	return typeof obj === 'object'
+}
+function isWindow(obj){
+	return obj!== null&&obj === obj.window;
+}
 
 function isPlainObject(obj){
-	return typeof obj === 'object' //&& 
+	return !isObject(obj) && !isWindow(obj) && Object.getPrototypeOf(obj) === Object.prototype;
 }
 
 function isArray(arr){
-	return typeof Array.isArray === 'function'? Array.isArray(arr):'';
+	return typeof Array.isArray === 'function'? Array.isArray(arr):arr instanceof Array;
 }
 
-function lookup(){
+
+
+
+function lookup(obj,path){
 	var lp = obj;
+	var tail = path.slice(-1);
+	var arrayIndex,tailArr;
+
+	if(/\w+:\d/.test(tail)){
+		tailArr = tail.split(':');
+		arrayIndex = tailArr[1];
+		path[path.length-1] = tailArr[0];
+	}
+
 	for(var i=0,len=path.length;i<len;i++){
-		if(!lp[p]){
-			throw new Error('blabla');
+		var p = path[i];
+
+		if(typeof lp[p] === 'undefined' && typeof lp[p][arrayIndex] === 'undefined'){
+			throw new Error('can not find this value');
 		}
-		lp = lp[p];
+
+		if(typeof arrayIndex !== 'undefined' && i === len-1){
+			lp = lp[p][arrayIndex];
+		}else{
+			lp = lp[p];
+		}
 	}
 	return lp;
 }
 
 
-function update(obj,value,path,command){
+function update(obj,path,value,command,config){
 	if(!isPlainObject(obj)){
 		throw new Error('obj should be a plain obj.');
 	}
@@ -29,16 +63,49 @@ function update(obj,value,path,command){
 		throw new Error('path should be an array.');
 	}
 	
-	doCommand(obj,value,path,command);
+	config = config || {merge:false};
+
+	doCommand(obj,value,path,command,config);
 }
 
 
-function doCommand(obj,lookup,value,command){
+function doCommand(obj,value,path,command,config){
 	switch(command){
-		case 'set': setfunc(obj,lookup,value);break;
-		case 'merge':mergeFunc(obj,lookup,value);break;
-		case 'push':pushFunc(obj,lookup,value);break;
+		case '$set': setfunc(obj,value,path,config);break;
+		case '$add':addFunc(obj,value,path,config);break;
+		case '$delete':deleteFunc(obj,value,path,config);break;
 	}
 }
 
 
+function setfunc(obj,value,path,config){
+	var lp = lookup(obj,path);
+
+	if(lp.constructor !== value.constructor){
+		immutableSet(obj,lp,path,value);
+	}else{
+		if(lp.constructor === Object && config.merge){
+			immutableMerge(obj,lp,path,value);
+		}else{
+			immutableSet(obj,lp,path,value);
+		}
+	}
+}
+
+function addFunc(obj,value,path,config){
+	var lp = lookup(obj,path);
+
+	if(!isArray(lp)){
+		throw new Error('the value pathed should be an array');
+	}
+
+	immutableAdd(obj,value,path);
+	
+}
+
+function deleteFunc(obj,value,path,config){
+	var lp = lookup(obj,path);
+
+	
+
+}
